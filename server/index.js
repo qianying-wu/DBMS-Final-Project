2eb9330f05ca75f1b87110163b15300a967b1dd2
Celine-MuMu <celine5640@gmail.com>
2026-05-07 01:30:23 +0800

    delete double things

--- server/index.js
+++ server/index.js
@@ -7,8 +7,6 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 const startPageDir = path.join(__dirname, 'views', 'StartPage');
 
 const mockUsers = {
