e2637853ec6c90f9655aa786c2b4b23639ed5fef
hsuyunling <113306005@g.nccu.edu.tw>
2026-05-08 13:32:00 +0800

    Delete: duplicated lines

--- server/index.js
+++ server/index.js
@@ -2,9 +2,6 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const app = express();
 const port = 3000;
 const __filename = fileURLToPath(import.meta.url);
